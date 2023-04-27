1636bba64f185b5950fa018b1c136d9cfb7f53c5
Serjdud <serjdudkin@gmail.com>
2023-04-27 10:52:37 +0300

    Bug fixes.

--- fan_control.py
+++ fan_control.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python
+#!/usr/bin/sudo /usr/bin/python3
 # -*- coding: utf-8 -*-
 
 import argparse
 import time
 import sys
+import logging
 
 # default values and arguments parsing
 parser = argparse.ArgumentParser(prog='Orange Pi Zero 2 fan control')
@@ -11,37 +12,50 @@ parser.add_argument('-n', '--pwm_num',
                     default='3',
                     type=int,
                     choices=[1, 2, 3, 4],
-                    help="One of 4 PWM. See orange pi zero 2 user manual")
-parser.add_argument('-p', '--init_power',
+                    metavar="NUMBER",
+                    help="Default %(default)s. One of 4 PWM. See orange pi zero 2 user manual.")
+parser.add_argument('-i', '--init_power',
                     default='0',
                     type=int,
                     choices=range(0, 101),
-                    help="Initial fan power in percent")
+                    metavar="PERCENT",
+                    help="Default %(default)s %%. Initial fan power [%%].")
 parser.add_argument('-f', '--pwm_freq',
                     default='1000',
                     type=int,
-                    help="PWM frequency")
+                    help="Default %(default)s Hz. PWM frequency [Hz].")
 parser.add_argument('-t', '--pwm_threshold',
                     default='350',
                     type=int,
-                    help="Minimal frequency when fan start to rotate")
+                    help="Default %(default)s Hz. Minimal frequency (Hz) when fan start to rotate.")
 parser.add_argument('--min_temp',
                     default='40',
                     type=int,
+                    metavar="MIN_TEMP",
                     choices=range(0, 100),
-                    help="Below this temp fan is off")
+                    help="Default %(default)s °C. Below this temp [°C] fan is off.")
 parser.add_argument('--max_temp',
                     default='60',
                     type=int,
+                    metavar="MAX_TEMP",
                     choices=range(0, 100),
-                    help="Above this temp fan is 100% power")
+                    help="Default %(default)s °C. Above this temp [°C] fan is 100 %% power.")
 parser.add_argument('--cycle_time',
                     default='10',
                     type=int,
-                    choices=range(0.1, 300),
-                    help="How often [s] temperature is measured and fan_control change fan power")
+                    metavar="CYCLE_TIME",
+                    choices=range(0, 300),
+                    help="Default %(default)s s. How often (sec) temperature is measured and fan power is changed.")
+parser.add_argument('-v', '--verbose',
+                    action='store_true',
+                    help="Enable verbose mode.")
 
 args = parser.parse_args()
+
+logging.basicConfig(format="%(levelname)s:%(message)s")
+if args.verbose:
+    logging.basicConfig(level=logging.INFO)
+
 PWM_NUMBER = args.pwm_num
 PWM_FREQ = args.pwm_freq
 PWM_FREQ_THRESHOLD = args.pwm_threshold
@@ -57,6 +71,7 @@ if PWM_FREQ_THRESHOLD >= PWM_FREQ:
     raise Exception("PWM frequency threshold must be <= pwm frequency")
 
 def set_fan_power(power_percent: int) -> None:
+    logging.info(f"FAN power = {power_percent}%.")
     if power_percent >= 100:
         pwm_duty_cycle = PWM_FREQ
     elif power_percent <= 0:
@@ -64,37 +79,53 @@ def set_fan_power(power_percent: int) -> None:
     else:
         pwm_duty_cycle = ((power_percent / 100) * (PWM_FREQ - PWM_FREQ_THRESHOLD)
                               + PWM_FREQ_THRESHOLD)
+    logging.info(f"PWM duty cycle = {pwm_duty_cycle} Hz")
     with open(f"/sys/class/pwm/pwmchip0/pwm{PWM_NUMBER}/duty_cycle", "w+") as dcf:
             dcf.write(str(pwm_duty_cycle))
+    logging.info(f"PWM duty cycle is setted to {pwm_duty_cycle} Hz")
 
 def pwm_turn_on() -> None:
-    with open("/sys/class/pwm/pwmchip0/export", "w+") as export_f:
-        export_f.write(str(PWM_NUMBER))
+    logging.info(f"trying to turn on pwm")
+    try:
+        with open("/sys/class/pwm/pwmchip0/export", "w+") as export_f:
+            export_f.write(str(PWM_NUMBER))
+        logging.info(f"PWM number is setted {PWM_NUMBER}.")
+    except OSError as err:
+        logging.warning("OS Error!")
+        logging.warning(err)
+    
     with open(f"/sys/class/pwm/pwmchip0/pwm{PWM_NUMBER}/period", "w+") as period_f:
         period_f.write(str(PWM_FREQ))
+    logging.info(f"PWM frequency is settet to {PWM_FREQ} Hz.")
+
     set_fan_power(INITIAL_FAN_POWER)
+
     with open(f"/sys/class/pwm/pwmchip0/pwm{PWM_NUMBER}/enable", "w+") as enable_f:
         enable_f.write("1")
+    logging.info("PWM turned on.")
 
 def pwm_turn_off() -> None:
     set_fan_power(0)
     with open(f"/sys/class/pwm/pwmchip0/pwm{PWM_NUMBER}/enable", "w+") as enable_f:
         enable_f.write("0")
+    logging.info("PWM turned off.")
 
 def get_cpu_max_temp() -> int:
-    cpu_temp = []
+    cpu_temp = [0]*4
     for i in range(0, 4):
         with open(f"/sys/class/thermal/thermal_zone{i}/temp", "r") as temp_f:
             cpu_temp[i] = int(temp_f.readline())
+        logging.info(f"Raw cpu temp values: {cpu_temp}")
     return max(cpu_temp) / 1000
 
-if __name__ == 'fan_control.py':
+if __name__ == '__main__':
     try:
         temp_step = (MAX_CPU_TEMP - MIN_CPU_TEMP) / 100
         pwm_turn_on()
 
         while True:
             cpu_temp = get_cpu_max_temp()
+            logging.info(f"CPU max temperature = {cpu_temp} ")
 
             if cpu_temp >= MAX_CPU_TEMP:
                 fan_power_percent = 100
@@ -103,9 +134,11 @@ if __name__ == 'fan_control.py':
             else:
                 fan_power_percent = ((cpu_temp - MIN_CPU_TEMP) / (MAX_CPU_TEMP - MIN_CPU_TEMP)) * 100
             
+            logging.info(f" FAN power = {fan_power_percent}%")
             set_fan_power(fan_power_percent)
             time.sleep(MEASUREMENT_FREQ)
     
     except KeyboardInterrupt:
         pwm_turn_off()
+        logging.info("Exit...")
         sys.exit()
--- fan_control.sh
+++ fan_control.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$path"
-sudo python3 ./fan_control.py
+
+sudo chmod o+rw /sys/class/pwm/pwmchip0/export
+sudo chmod o+rw /sys/class/pwm/pwmchip0/pwm*/period
+sudo chmod o+rw /sys/class/pwm/pwmchip0/pwm*/duty_cycle
+sudo chmod o+rw /sys/class/pwm/pwmchip0/pwm*/enable
+sudo chmod o+r /sys/class/thermal/thermal_zone*/temp
+
+sudo chmod +x ./fan_control.py
+sudo python3 ./fan_control.py "$@"
